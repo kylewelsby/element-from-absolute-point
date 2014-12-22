cf307493f8164e9e3fd40ad44ed202a2ef87b9f8
Kyle Welsby <kyle@mekyle.com>
2014-12-22 22:13:28 +0800

    ignore `position:fixed` elements.

--- element-from-absolute-point.js
+++ element-from-absolute-point.js
@@ -1,15 +1,23 @@
 (function() {
   'use strict';
   var api;
-  api = function(x,y) {
+  api = function(x, y) {
     var elm, scrollX, scrollY, newX, newY;
     scrollX = window.pageXOffset;
     scrollY = window.pageYOffset;
-    window.scrollTo(x,y);
+    window.scrollTo(x, y);
     newX = x - window.pageXOffset;
     newY = y - window.pageYOffset;
-    elm = this.elementFromPoint(newX,newY);
-    window.scrollTo(scrollX,scrollY);
+    elm = this.elementFromPoint(newX, newY);
+    if (elm.style.position === 'fixed') {
+      var newElm, display;
+      display = elm.style.display;
+      elm.style.display = 'none';
+      newElm = this.elementFromPoint(newX, newY);
+      elm.style.display = display;
+      elm = newElm;
+    }
+    window.scrollTo(scrollX, scrollY);
     return elm;
   };
   this.document.elementFromAbsolutePoint = api;
--- element-from-absolute-point_test.js
+++ element-from-absolute-point_test.js
@@ -2,7 +2,20 @@
   'use strict';
   describe('document.elementFromAbsolutePoint', function () {
     var elm, pointX, pointY;
+    function addPositionFixed(){
+      var elm = document.createElement('div');
+      elm.innerHTML = '';
+      elm.style.position = 'fixed';
+      elm.style.top = '0px';
+      elm.style.left = '0px';
+      elm.style.zIndex = '101';
+      elm.style.width = '100%';
+      elm.style.height = '100px';
+      elm.style.backgroundColor = '#000';
+      document.getElementById('jasmine_content').appendChild(elm);
+    }
     beforeEach(function() {
+      document.getElementById('jasmine_content').innerHTML = '';
       pointX = 1000;
       pointY = pointX * 2;
       document.body.style.margin = '0';
@@ -11,6 +24,7 @@
       elm.innerHTML = 'hello world';
       elm.style.marginTop = pointY + 'px';
       elm.style.marginLeft = pointX +'px';
+      elm.style.marginBottom = '1000px';
 
       document.getElementById('jasmine_content').appendChild(elm);
     });
@@ -52,6 +66,16 @@
       actual = document.elementFromAbsolutePoint(rec.left, rec.top)
       expect(actual).not.toBe(null);
       expect(actual.toString()).toBe('[object HTMLDivElement]');
+    });
+
+    it('gets an element excluding position fixed', function (){
+      addPositionFixed();
+      var actual, rec;
+      rec = elm.getBoundingClientRect();
+      actual = document.elementFromAbsolutePoint(rec.left, rec.top);
+      expect(actual).not.toBe(null);
+      console.log(actual.textContent);
+      expect(actual.textContent).toEqual('hello world');
     });
   });
 }).call(this);
